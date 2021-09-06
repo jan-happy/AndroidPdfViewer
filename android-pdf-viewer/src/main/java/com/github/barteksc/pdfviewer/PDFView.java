08150bf9038396ae31e9e984abcd0a7a29843676
Jan Frölich <695826+jan-happy@users.noreply.github.com>
2021-09-06 16:03:53 +0200

    Corrected the offset

--- android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/PDFView.java
+++ android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/PDFView.java
@@ -344,7 +344,7 @@ public class PDFView extends RelativeLayout {
         page = pdfFile.determineValidPageNumberFrom(page);
         float offset = page == 0 ? 0 : -pdfFile.getPageOffset(page, zoom);
                 offset += pdfFile.getPageSpacing(page, getZoom()) / 2f;
-        Log.v("JUMP_TO_OFFSET", String.valueOf(offset));
+        Log.v("JUMP_TO_OFFSET NEW", String.valueOf(offset));
         if (swipeVertical) {
             if (withAnimation) {
                 animationManager.startYAnimation(currentYOffset, offset);
